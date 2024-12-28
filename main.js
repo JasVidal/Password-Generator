d2d0a12273dc11dc201e93486de75dc1b4787cad
Jasminvn <160454837+JasVidal@users.noreply.github.com>
2024-12-28 00:53:23 -0500

    actualizar: se agregaron símbolos especiales para crear contraseñas más seguras, se implementó un botón para limpiar el campo de la contraseña

--- main.js
+++ main.js
@@ -1,9 +1,40 @@
+let passwordEntered = document.getElementById('cantidad');
+let buttonGenerate = document.getElementById('generar');
+let buttonClear = document.getElementById('limpiar');
+let passwordGenerated = document.getElementById('contrasena');
 
+const characterString = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
 
+function generatePassword(){
 
+    let numberEntered = parseInt(passwordEntered.value);
 
+    if (numberEntered < 8){
+        alert('Los caracteres deben ser mayor que 8');
+    }
 
+    /*Construir la contraseña*/
 
+    let password ='';
+    /*Inicializador, Condición, Resultado */
+    for(let i = 0; i < numberEntered; i++){
 
+        let randomCharacter = characterString[Math.floor(Math.random() * characterString.length)];
+        
+        password+=randomCharacter;
+    }
 
+    passwordGenerated.value = password;
+}
+
+/* ------- Función para limpiar los campos ------- */
+
+function clearFields() {
+    passwordEntered.value = '';
+    passwordGenerated.value = '';
+}
+
+// Se asigna la función al botón
+
+buttonClear.addEventListener('click', clearFields)
 
